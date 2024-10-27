1a52a1d9f3a6fac1f4370227e7f7ef266ce01874
JainishParekh <jainishparekh21@gmail.com>
2024-10-28 01:12:50 +0530

    updated custom page and app

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
+import Hero from "./components/hero/Hero"
+import Navbar from "./components/navbar/navbar"
 import CustomPage from "./custom-components/custom_page"
+import Parallax from "./components/parallex/Parallex"
+import Skills from "./components/skills/Skills"
+import Portfolio from "./components/portfolio/Portfolio"
 
 function App() {
 
   return (
     <>
       <div className="snap-y scroll-smooth main_wrapper">
-        <CustomPage>Hero</CustomPage>
-        <CustomPage>Parallex</CustomPage>
-        <CustomPage>Services</CustomPage>
-        <CustomPage>Parallex</CustomPage>
-        <CustomPage>Portfolio1</CustomPage>
-        <CustomPage>Portfolio2</CustomPage>
-        <CustomPage>Portfolio3</CustomPage>
-        <CustomPage>Contact</CustomPage>
+        <CustomPage className={`flex flex-col`} id="homePage">
+          <Navbar />
+          <Hero />
+        </CustomPage>
+        <CustomPage><Parallax type="skills"></Parallax></CustomPage>
+        <CustomPage><Skills /></CustomPage>
+        <CustomPage><Parallax ></Parallax></CustomPage>
+        <Portfolio />
+        {/* <CustomPage>Contact</CustomPage> */}
       </div>
     </>
   )
--- src/custom-components/custom_page.jsx
+++ src/custom-components/custom_page.jsx
@@ -1,8 +1,8 @@
 
 
-function CustomPage({ children }) {
+function CustomPage({ children, className = "", ...props }) {
     return (
-        <section className="bg-primary text-gray-400 h-[100vh] snap-center">
+        <section className={` bg-primary text-gray-400 h-[100vh] snap-center overflow-hidden ${className} custom_page_wrapper`} {...props}>
             {children}
         </section>
     )
